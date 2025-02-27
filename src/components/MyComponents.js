15e459ab0e066847d5ad8a4437c1f6e0537d140c
TriHuynh-IT <huynhtri06it@gmail.com>
2025-02-27 14:13:55 +0700

    video16

---
+++ src/components/MyComponents.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        my component
+        {Math.random()}
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
